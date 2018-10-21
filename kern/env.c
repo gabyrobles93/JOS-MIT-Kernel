35bd7d3d19f4bddfdf814a57d81808bb2af71f1a
atix <aalvarez@atixlabs.com>
2018-10-20 21:15:18 -0300

    Agrego tarea env_init, 0% testeada

--- kern/env.c
+++ kern/env.c
@@ -113,7 +113,12 @@ env_init(void)
 {
 	// Set up envs array
 	// LAB 3: Your code here.
-
+  for (int i = NENV - 1 ; i >= 0 ; i--) {
+    envs[i].env_link = env_free_list;
+    // envs[i].env_id = 0; // Hecho por memset
+    // envs[i].env_status = ENV_FREE // Hecho por memset
+    env_free_list = &envs[i];
+  } 
 	// Per-CPU part of the initialization
 	env_init_percpu();
 }
