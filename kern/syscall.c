ac3195573651030a51e78eef7ba56c22fbdc5a16
gabyrobles93 <gabyrobles93@gmail.com>
2018-11-10 18:37:48 -0300

    Agrego la entrada de la nueva syscall al Switch, esta vez no me olvide.

--- kern/syscall.c
+++ kern/syscall.c
@@ -541,6 +541,11 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	case SYS_ipc_try_send: {
 		return (int) sys_ipc_try_send((envid_t)a1, (uint32_t)a2, (void *)a3, (unsigned)a4);
 	}
+
+	case SYS_env_set_pgfault_upcall: {
+		return (int) sys_env_set_pgfault_upcall((envid_t) a1, (void *) a2);
+	}
+
 	default:
 		return -E_INVAL;
 	}
