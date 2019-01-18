c27b28dac16dc5bbb4eac5b436ccab71b9b383fa
gabyrobles93 <gabyrobles93@gmail.com>
2019-01-17 23:01:13 -0300

    Exercise 3: Agrego entrada en pci_attach_vendor de la placa de red con el Vendor ID, Device ID y attachfn. El attachfn es una función de inicialización del dispositivo que creé en el archivo e1000.c-h. Pasa la prueba.

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,9 @@
 #include <kern/e1000.h>
 
 // LAB 6: Your driver code here
+
+int e1000_init(struct pci_func *pcif) {
+    pci_func_enable(pcif);
+
+    return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,9 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <inc/types.h>
+#include <kern/pci.h>
+
+int e1000_init(struct pci_func *pcif);
+
 #endif  // JOS_KERN_E1000_H
