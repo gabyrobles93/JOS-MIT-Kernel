1657f851f4191c913bc0be94075927e673f420ac
gabyrobles93 <gabyrobles93@gmail.com>
2019-01-17 23:03:29 -0300

    Agrego comentarios informativos.

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,6 +2,9 @@
 
 // LAB 6: Your driver code here
 
+// Inicializa la placa de red, es invocada por el código de PCI (kern/pci.c) que detecta el la placa de red
+// en el puerto PCI del motherboard y luego recorre el arreglo de dispositivos PCI
+// y matchea Vendor y Device ID para invocar a esta función de inicialización.
 int e1000_init(struct pci_func *pcif) {
     pci_func_enable(pcif);
 
